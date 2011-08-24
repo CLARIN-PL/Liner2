a58228f4c47e7c9f90ba7627efa61b67a1290d4b
Michał Marcińczuk <marcinczuk@gmail.com>
2011-08-24 11:32:23 +0200

    Dodanie klas Paragraph i ParagraphSet

---
+++ src/liner2/structure/Paragraph.java
@@ -0,0 +1,11 @@
+package liner2.structure;
+
+import java.util.ArrayList;
+
+public class Paragraph {
+
+	private String id = null;
+	
+	private ArrayList<Sentence> sentences = new ArrayList<Sentence>(); 
+	
+}
---
+++ src/liner2/structure/ParagraphsSet.java
@@ -0,0 +1,9 @@
+package liner2.structure;
+
+import java.util.ArrayList;
+
+public class ParagraphsSet {
+
+	ArrayList<Paragraph> paragraphs = new ArrayList<Paragraph>();
+	
+}
