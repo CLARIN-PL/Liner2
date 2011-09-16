5ca1b505fdad40b81620bc183c6c54014a2ef8db
Maciej Janicki <macjan@o2.pl>
2011-09-16 12:48:40 +0200

    Generowanie złożonych cech w szablonach (#2294).

--- src/liner2/tools/Template.java
+++ src/liner2/tools/Template.java
@@ -10,19 +10,47 @@ public class Template {
 	Hashtable<String, String[]> features = new Hashtable<String, String[]>();
 	
 	public void addFeature(String description) throws Exception {
-		int pos = description.indexOf(":");
-		if (pos == -1)
-			throw new Exception("Invalid template description: "+description);
-		String featureName = description.substring(0, pos);
-		String[] windowDesc = description.substring(pos+1).split(":");
-		if (this.features.containsKey(featureName))
-			throw new Exception("Duplicate feature definition in template description: "+description);
-		else {
-			this.featureNames.add(featureName);
-			this.features.put(featureName, windowDesc);
+		String[] featureUnits = description.split("/");
+		if (featureUnits.length < 1)
+			throw new Exception("Invalid template description: " + description);
+		// cecha "pojedyncza": featureNames <= nazwa, features <= nazwa, opis okna
+		else if (featureUnits.length == 1) {
+			int pos = featureUnits[0].indexOf(":");
+			if (pos == -1)
+				throw new Exception("Invalid template description: " + description);
+			String featureName = featureUnits[0].substring(0, pos);
+			String[] windowDesc = featureUnits[0].split(":");
+			if (this.features.containsKey(featureName))
+				throw new Exception("Duplicate feature definition in template description: "+description);
+			else {
+				this.featureNames.add(featureName);
+				this.features.put(featureName, windowDesc);
+			}
 		}
-		
-		Main.log("Added feature "+featureName+" with description "+windowDesc);
+		// cecha złożona:
+		// featureNames <= pełna nazwa cechy
+		// features <= pary: nazwa_pojedynczej_cechy, pozycja
+		else {
+			StringBuilder featureNameB = new StringBuilder("");
+			String[] windowDesc = new String[featureUnits.length * 2];
+			for (int i = 0; i < featureUnits.length; i++) {
+				String[] featureUnit = featureUnits[i].split(":");
+				if (featureUnit.length != 2)
+					throw new Exception("Invalid template description: " + description);
+				if (featureNameB.length() > 0)
+					featureNameB.append("/");
+				featureNameB.append(featureUnit[0] + "[" + featureUnit[1] + "]");
+				windowDesc[i*2] = featureUnit[0];
+				windowDesc[i*2+1] = featureUnit[1];
+			}
+			String featureName = featureNameB.toString();
+			if (this.features.containsKey(featureName))
+				throw new Exception("Duplicate feature definition in template description: "+description);
+			else {
+				this.featureNames.add(featureName);
+				this.features.put(featureName, windowDesc);
+			}
+		}
 	}
 	
 	public ArrayList<String> getFeatureNames() {
--- src/liner2/tools/TemplateFactory.java
+++ src/liner2/tools/TemplateFactory.java
@@ -65,17 +65,39 @@ public class TemplateFactory {
 		Hashtable<String, String[]> features = template.getFeatures();
 		for (String featureName : template.getFeatureNames()) {
 			pw.write("# " + featureName + "\n");
-			String featureId = Integer.toString(attributeIndex.getIndex(featureName));
-			String featureIdFixed = featureId;
-			while (featureIdFixed.length() < 2)
-				featureIdFixed = "0" + featureIdFixed;
 			String[] windowDesc = features.get(featureName);
-			for (String w : windowDesc) {
-				String wFixed = w;
-				if (!wFixed.startsWith("-"))
-					wFixed = "+" + wFixed;
-				pw.write("U" + featureIdFixed + wFixed 
-					+ ":%x[" + w + "," + featureId + "]\n");
+			// cecha pojedyncza
+			if (featureName.equals(windowDesc[0])) {
+				String featureId = Integer.toString(attributeIndex.getIndex(featureName));
+				String featureIdFixed = featureId;
+				while (featureIdFixed.length() < 2)
+					featureIdFixed = "0" + featureIdFixed;
+				for (int i = 1; i < windowDesc.length; i++) {
+					String wFixed = windowDesc[i];
+					if (!wFixed.startsWith("-"))
+						wFixed = "+" + wFixed;
+					pw.write("U" + featureIdFixed + wFixed 
+						+ ":%x[" + windowDesc[i] + "," + featureId + "]\n");
+				}
+			}
+			// cecha złożona
+			else {
+				String unigramId = "U";
+				String unigramContent = "";
+				for (int i = 0; i < windowDesc.length - 1; i += 2) {
+					if (unigramId.length() > 1) unigramId += "/";
+					if (unigramContent.length() > 0) unigramContent += "/";
+					String featureId = Integer.toString(attributeIndex.getIndex(windowDesc[i]));
+					String featureIdFixed = featureId;
+					while (featureIdFixed.length() < 2)
+						featureIdFixed = "0" + featureIdFixed;
+					String wFixed = windowDesc[i+1];
+					if (!wFixed.startsWith("-"))
+						wFixed = "+" + wFixed;
+					unigramId += featureIdFixed + wFixed;
+					unigramContent += "%x[" + windowDesc[i+1] + "," + featureId + "]";
+				}
+				pw.write(unigramId + ":" + unigramContent + "\n");
 			}
 			pw.write("\n");
 		}
