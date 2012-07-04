44df85b1336f25fdcf7586a8e0dd4e84dcf17128
Pawel Minda <pawel.minda@gmail.com>
2012-07-04 09:52:17 +0200

    Print lines removed

--- src/liner2/action/ActionBatchFile.java
+++ src/liner2/action/ActionBatchFile.java
@@ -37,42 +37,35 @@ public class ActionBatchFile extends Action{
 		if ( !LinerOptions.isOption(LinerOptions.OPTION_IS) ){
                         throw new ParameterException("Parameter --is <file_with_input_files_list> not set");
                 }
-		System.out.println(LinerOptions.getOption(LinerOptions.OPTION_USE));
 		ChunkerFactory.loadChunkers(LinerOptions.get().chunkersDescription);
 		Chunker chunker = ChunkerFactory.getChunkerPipe(LinerOptions.getOption(LinerOptions.OPTION_USE));
 
 		File file = null;
   		FileReader freader = null;
   		LineNumberReader lnreader = null;
-		System.out.println("1");
   		try{
   			file = new File(LinerOptions.getOption(LinerOptions.OPTION_IS));
   			freader = new FileReader(file);
   			lnreader = new LineNumberReader(freader);
   			String line = "";
-			System.out.println("2");
 			while ((line = lnreader.readLine()) != null){
 				StreamReader reader = ReaderFactory.get().getStreamReader(
                         		line,
                         		LinerOptions.getOption(LinerOptions.OPTION_INPUT_FORMAT));
                 		ParagraphSet ps = reader.readParagraphSet();
-				System.out.println("3");
 				for (Paragraph p : ps.getParagraphs())
                         		for (Sentence s : p.getSentences())
                                 		chunker.chunkSentenceInPlace(s);
-				System.out.println("4");
                 		StreamWriter writer = WriterFactory.get().getStreamWriter(
                         		line+"."+LinerOptions.getOption(LinerOptions.OPTION_OUTPUT_FORMAT),
                         		LinerOptions.getOption(LinerOptions.OPTION_OUTPUT_FORMAT));
                 		writer.writeParagraphSet(ps);
-				System.out.println("5");
 			}
 		}
 		finally{
   			freader.close();
   			lnreader.close();
   		}
-		System.out.println("6");
 		
 		/* Create all defined chunkers. */
 		
