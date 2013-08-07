9ebfc906436e61bf32df20b6033d80f793a66970
michal <michal@rabarbar.(none)>
2013-08-07 13:37:40 +0200

    zmiana sposobu tworzenia/ladowania klasyfiaktora anotacji

---
+++ src/liner2/chunker/factory/ChunkerFactoryItemAnnotationClassifier.java
@@ -0,0 +1,83 @@
+package liner2.chunker.factory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+
+import liner2.Main;
+import liner2.chunker.AnnotationClassifierChunker;
+import liner2.chunker.Chunker;
+import liner2.chunker.TrainableChunkerInterface;
+import liner2.reader.ReaderFactory;
+import liner2.reader.StreamReader;
+import liner2.tools.CorpusFactory;
+import liner2.tools.ParameterException;
+import org.ini4j.Ini;
+
+public class ChunkerFactoryItemAnnotationClassifier extends ChunkerFactoryItem {
+
+	public ChunkerFactoryItemAnnotationClassifier() {
+		super("classifier:([^:]*)(:base=(.*))?");
+	}
+
+	@Override
+	public Chunker getChunker(String description) throws Exception {
+		Main.log("Training annotation classifier");
+
+		Matcher m = this.pattern.matcher(description);
+		
+		if ( !m.find() )
+			return null;
+        String iniPath = m.group(1);
+        String iniDir = new File(iniPath).getParent();
+
+        String inputClassifier = m.group(3);
+        Ini ini = new Ini(new FileReader(iniPath));
+        Ini.Section main = ini.get("main");
+        Ini.Section classifierDesc = ini.get("classifier");
+        Ini.Section dataDesc = ini.get("data");
+        Ini.Section featuresDesc = ini.get("features");
+
+        Chunker baseChunker = null;
+		
+		if ( inputClassifier != null ){
+			baseChunker = ChunkerFactory.getChunkerByName(inputClassifier);
+			if (baseChunker == null)
+				throw new ParameterException("Annotation Classifier: undefined base chunker: " + inputClassifier);
+		}
+
+        List<String> features = new ArrayList<String>();
+        for(String featureName: featuresDesc.keySet())
+        features.add(featuresDesc.get(featureName).replace("{INI_DIR}",iniDir));
+
+		AnnotationClassifierChunker chunker = new AnnotationClassifierChunker(baseChunker, features);
+
+        String mode = main.get("mode");
+        String modelPath = main.get("store").replace("{INI_DIR}", iniDir);
+        File modelFile = new File(modelPath);
+        if ( mode.equals("load") && modelFile.exists()){
+            chunker.deserialize(modelPath);
+        }
+        else {
+            String inputFormat = dataDesc.get("format");
+            String inputFile = dataDesc.get("source").replace("{INI_DIR}",iniDir);
+
+            if ((inputFormat.equals("iob")) || (inputFormat.equals("ccl"))) {
+                Main.log("--> Training on file=" + inputFile);
+                StreamReader reader = ReaderFactory.get().getStreamReader(inputFile, inputFormat);
+                ((TrainableChunkerInterface)chunker).train(reader.readParagraphSet());
+            }
+            else {
+                Main.log("--> Training on corpus=" + inputFile);
+                ((TrainableChunkerInterface)chunker).train(CorpusFactory.get().query(inputFile));
+            }
+            modelFile.createNewFile();
+            chunker.serialize(modelPath);
+        }
+		return chunker;
+	}
+
+}
