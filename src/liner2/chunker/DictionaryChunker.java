182426da5fd7afcebfe1a07d31699de27ad4fc26
Michal Krautforst <michal@Glados.(none)>
2014-06-17 16:44:03 +0200

    współdzielenie wczytanych słowników dla cech i chunkerów

--- src/liner2/chunker/DictionaryChunker.java
+++ src/liner2/chunker/DictionaryChunker.java
@@ -1,12 +1,6 @@
 package liner2.chunker;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -31,6 +25,9 @@ import liner2.Main;
 
 public class DictionaryChunker extends Chunker 
 	implements DeserializableChunkerInterface, SerializableChunkerInterface {
+
+    static HashMap<String, HashMap<String, String>> loadedDicts = new HashMap<String, HashMap<String, String>>();
+    static HashMap<String, HashSet<String>> loadedCommons = new HashMap<String, HashSet<String>>();
 	
 	private HashMap<String, String> dictionary = null;
 	private HashSet<String> commons = null;
@@ -105,55 +102,63 @@ public class DictionaryChunker extends Chunker
 	}
 	
 	public void loadDictionary(String dictFile, String commonsFile) {
-		try {
-			BufferedReader commonsReader = new BufferedReader(new FileReader(commonsFile));
-			HashSet<String> ambigous = new HashSet<String>();
-			
-			String line = commonsReader.readLine();
-			while (line != null) {
-				this.commons.add(line.trim());
-				line = commonsReader.readLine();
-			}
-			commonsReader.close();
-			
-			BufferedReader dictReader = new BufferedReader(new FileReader(dictFile));
-			line = dictReader.readLine();
-			Pattern pattern = Pattern.compile("\\d+");
-			int added = 0, amb = 0, com = 0;
-			while (line != null) {
-				String[] content = line.split("\t");
-				if (content.length >= 2) {
-			    	Matcher m = pattern.matcher(content[1]);
-					if ((commons.contains(content[1])) || (commons.contains(content[1].toLowerCase())))
-						com++;
-					else if (ambigous.contains(content[1]))
-						amb++;
-					else if (m.matches())
-						com++;
-					else if ((this.dictionary.containsKey(content[1])) && 
-							(!this.dictionary.get(content[1]).equals(content[0]))){
-						this.dictionary.remove(content[1]);
-						ambigous.add(content[1]);
-						amb += 2;
-						added -= 1;
-					}
-					else {
-						content[1] = content[1].replaceAll("\\.", " \\. ").replaceAll("\\-", " \\- ")
-							.replaceAll("\\'", " \\' ").replaceAll("\\\"", " \\\" ")
-							.replaceAll(" +", " ").trim();
-						this.dictionary.put(content[1], content[0]);
-						added++;
-					}
-				}
-				line = dictReader.readLine();
-			}
-			Main.log("Dictionary chunker compiled.", true);
-			Main.log("Added: " + added, true);
-			Main.log("Skipped: " + (amb+com) + " of which " + amb + " ambigous, " + com + " common.", true);
-			dictReader.close();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
+        String dictName = new File(dictFile).getName();
+        if(loadedDicts.containsKey(dictName)){
+            this.dictionary = loadedDicts.get(dictName);
+            this.commons = loadedCommons.get(dictName);
+        }
+        else {
+            try {
+                BufferedReader commonsReader = new BufferedReader(new FileReader(commonsFile));
+                HashSet<String> ambigous = new HashSet<String>();
+
+                String line = commonsReader.readLine();
+                while (line != null) {
+                    this.commons.add(line.trim());
+                    line = commonsReader.readLine();
+                }
+                commonsReader.close();
+
+                BufferedReader dictReader = new BufferedReader(new FileReader(dictFile));
+                line = dictReader.readLine();
+                Pattern pattern = Pattern.compile("\\d+");
+                int added = 0, amb = 0, com = 0;
+                while (line != null) {
+                    String[] content = line.split("\t");
+                    if (content.length >= 2) {
+                        Matcher m = pattern.matcher(content[1]);
+                        if ((commons.contains(content[1])) || (commons.contains(content[1].toLowerCase())))
+                            com++;
+                        else if (ambigous.contains(content[1]))
+                            amb++;
+                        else if (m.matches())
+                            com++;
+                        else if ((this.dictionary.containsKey(content[1])) &&
+                                (!this.dictionary.get(content[1]).equals(content[0]))) {
+                            this.dictionary.remove(content[1]);
+                            ambigous.add(content[1]);
+                            amb += 2;
+                            added -= 1;
+                        } else {
+                            content[1] = content[1].replaceAll("\\.", " \\. ").replaceAll("\\-", " \\- ")
+                                    .replaceAll("\\'", " \\' ").replaceAll("\\\"", " \\\" ")
+                                    .replaceAll(" +", " ").trim();
+                            this.dictionary.put(content[1], content[0]);
+                            added++;
+                        }
+                    }
+                    line = dictReader.readLine();
+                }
+                Main.log("Dictionary chunker compiled.", true);
+                Main.log("Added: " + added, true);
+                Main.log("Skipped: " + (amb + com) + " of which " + amb + " ambigous, " + com + " common.", true);
+                dictReader.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            loadedDicts.put(dictName, this.dictionary);
+            loadedCommons.put(dictName, this.commons);
+        }
 	}
 	
 	public void addEntry(String name, String channel){
@@ -180,16 +185,25 @@ public class DictionaryChunker extends Chunker
 	@Override
 	@SuppressWarnings("unchecked")
     public void deserialize(String filename){
-    	try {
-    		ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
-   			this.dictionary = (HashMap<String, String>)in.readObject();
-   			this.commons = (HashSet<String>)in.readObject();
-    		in.close();
-    	} catch (ClassNotFoundException ex) {
-    		ex.printStackTrace();
-    	} catch (IOException ex) {
-    		ex.printStackTrace();
-    	}    	
+        String dictName = new File(filename).getName();
+        if(loadedDicts.containsKey(dictName)){
+            this.dictionary = loadedDicts.get(dictName);
+            this.commons = loadedCommons.get(dictName);
+        }
+        else{
+            try {
+                ObjectInputStream in = new ObjectInputStream(new FileInputStream(filename));
+                this.dictionary = (HashMap<String, String>)in.readObject();
+                this.commons = (HashSet<String>)in.readObject();
+                in.close();
+            } catch (ClassNotFoundException ex) {
+                ex.printStackTrace();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+            loadedDicts.put(dictName, this.dictionary);
+            loadedCommons.put(dictName, this.commons);
+        }
     }
 
 	@Override
--- src/liner2/features/tokens/DictFeature.java
+++ src/liner2/features/tokens/DictFeature.java
@@ -6,12 +6,14 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import liner2.structure.Token;
 import liner2.structure.Sentence;
 import liner2.tools.TrieDictNode;
 
 public class DictFeature extends TokenInSentenceFeature{
+    static HashMap<String, TrieDictNode> loadedDicts = new HashMap<String, TrieDictNode>();
 	
 	private TrieDictNode dict = new TrieDictNode(false);
 	private int sourceFeatureIdx;
@@ -27,29 +29,36 @@ public class DictFeature extends TokenInSentenceFeature{
 	}
 	
 	public void createDictFromFile(String path) throws IOException{
-		if (!new File(path).exists())
+        File dictFile = new File(path);
+		if (!dictFile.exists())
 			throw new FileNotFoundException("Invalid dictionary directory for feature "+name+": "+path);
-		BufferedReader inFile = new BufferedReader(new FileReader(path));
-		String entry = inFile.readLine();
-		String[] words;
-		while (entry != null){
-			
-			words = entry.split(" ");
-			if (words.length == 1)
-				this.dict.addChild(entry, true);
-			else{
-				int wordIdx = 0;
-				TrieDictNode dictNode = this.dict;
-				while ( wordIdx < words.length - 1){
-					dictNode.addChild(words[wordIdx], false);
-					dictNode = dictNode.getChild(words[wordIdx]);
-					wordIdx++;
-				}
-				dictNode.addChild(words[wordIdx], true);
-			}	
-			entry = inFile.readLine();
-		}	
-		inFile.close();
+        if(loadedDicts.containsKey(dictFile.getName())){
+            this.dict = loadedDicts.get(dictFile.getName());
+        }
+        else{
+            BufferedReader inFile = new BufferedReader(new FileReader(path));
+            String entry = inFile.readLine();
+            String[] words;
+            while (entry != null){
+
+                words = entry.split(" ");
+                if (words.length == 1)
+                    this.dict.addChild(entry, true);
+                else{
+                    int wordIdx = 0;
+                    TrieDictNode dictNode = this.dict;
+                    while ( wordIdx < words.length - 1){
+                        dictNode.addChild(words[wordIdx], false);
+                        dictNode = dictNode.getChild(words[wordIdx]);
+                        wordIdx++;
+                    }
+                    dictNode.addChild(words[wordIdx], true);
+                }
+                entry = inFile.readLine();
+            }
+            inFile.close();
+            loadedDicts.put(dictFile.getName(), this.dict);
+        }
 	}
 
 	@Override
