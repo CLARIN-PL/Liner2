9f946bab5dc94239256f68c6d0fa44bf2a75c085
Michał Marcińczuk <marcinczuk@gmail.com>
2014-06-24 15:25:19 +0200

    Fizyczny podział na moduły.

--- liner2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Wrap command to execure liner2
-
-# Get liner2 location
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-# Run liner2
-java -Xmx4024M -cp $DIR/lib/mysql-connector-java-5.1.10.jar -Djava.library.path=$DIR/lib:/lib -jar $DIR/liner2.jar $@
