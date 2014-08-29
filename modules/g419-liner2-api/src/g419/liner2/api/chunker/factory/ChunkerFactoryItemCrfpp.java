8e72f5c6840931600dd06e5c0f4acfcd7ef64280
Michal Krautforst <michal@Glados.(none)>
2014-08-29 14:02:00 +0200

    wczytywanie template'u do globalnych opcji

--- modules/g419-liner2-api/src/g419/liner2/api/chunker/factory/ChunkerFactoryItemCrfpp.java
+++ modules/g419-liner2-api/src/g419/liner2/api/chunker/factory/ChunkerFactoryItemCrfpp.java
@@ -105,7 +105,14 @@ public class ChunkerFactoryItemCrfpp extends ChunkerFactoryItem {
         TokenFeatureGenerator gen = new TokenFeatureGenerator(cm.opts.features);
 
         String templateData = main.get("template").replace("{INI_PATH}", iniDir);
-        CrfTemplate template = TemplateFactory.parseTemplate(templateData);
+        CrfTemplate template;
+        if(!LinerOptions.getGlobal().templates.containsKey(templateData)){
+            template = TemplateFactory.parseTemplate(templateData);
+            LinerOptions.getGlobal().templates.put(templateData, template);
+        }
+        else{
+            template =  LinerOptions.getGlobal().templates.get(templateData);
+        }
         File templateFile = File.createTempFile("template", ".tpl");
         CrfppChunker chunker = new CrfppChunker(threads, types);
         chunker.setTemplateFilename(templateFile.getAbsolutePath());
