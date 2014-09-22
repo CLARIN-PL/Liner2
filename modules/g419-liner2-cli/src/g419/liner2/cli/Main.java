c0e6e598c041e5323ee0cff52289d13f8a772e6e
Michal Krautforst <michal@glados>
2014-09-22 12:04:03 +0200

    usuniecie stacktrace'a z rzucanych wyjatkow

--- modules/g419-liner2-cli/src/g419/liner2/cli/Main.java
+++ modules/g419-liner2-cli/src/g419/liner2/cli/Main.java
@@ -71,7 +71,6 @@ public class Main {
 				}
                 catch (Exception e) {
                     System.out.println(e);
-                    e.printStackTrace();
                 }
     		}
     	}
