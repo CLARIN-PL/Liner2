b43c3dbcc82c383f3ff706734bd9fd9f4c267e26
Michal Krautforst <michal@glados>
2014-10-03 10:41:00 +0200

    obsluga komentarzy w pliku z mapowaniem anotacji

--- modules/g419-liner2-api/src/g419/liner2/api/converter/AnnotationMappingConverter.java
+++ modules/g419-liner2-api/src/g419/liner2/api/converter/AnnotationMappingConverter.java
@@ -85,9 +85,11 @@ public class AnnotationMappingConverter extends Converter{
         try {
             String line = br.readLine();
             while (line != null) {
-                String [] pattern_val = line.split(" -> ");
-                channelsMapping.add(new PatternStringEntry(Pattern.compile("^"+pattern_val[0]+"$"), pattern_val[1]));
-                line = br.readLine();
+                if(line.isEmpty() || !line.startsWith("#")) {
+                    String[] pattern_val = line.split(" -> ");
+                    channelsMapping.add(new PatternStringEntry(Pattern.compile("^" + pattern_val[0] + "$"), pattern_val[1]));
+                    line = br.readLine();
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
