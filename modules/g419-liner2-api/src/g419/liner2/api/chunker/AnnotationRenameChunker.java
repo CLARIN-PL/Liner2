99498c466ec0494c264f4412a27fb57876cf0f9c
Michał Marcińczuk <marcinczuk@gmail.com>
2015-03-23 15:41:37 +0100

    Implementacja AnnotationRenameChunker.

--- modules/g419-liner2-api/src/g419/liner2/api/chunker/AnnotationRenameChunker.java
+++ modules/g419-liner2-api/src/g419/liner2/api/chunker/AnnotationRenameChunker.java
@@ -1,5 +1,6 @@
 package g419.liner2.api.chunker;
 
+import g419.corpus.structure.Annotation;
 import g419.corpus.structure.AnnotationSet;
 import g419.corpus.structure.Document;
 import g419.corpus.structure.Sentence;
@@ -13,8 +14,9 @@ public class AnnotationRenameChunker extends Chunker {
 	private Chunker chunker = null;
 	private Map<String, String> rename = new HashMap<String, String>();
 
-    public AnnotationRenameChunker(Chunker baseChunker) {
+    public AnnotationRenameChunker(Chunker baseChunker, Map<String, String> rename) {
 		this.chunker = baseChunker;
+		this.rename = rename;
     }
     
     /**
@@ -26,10 +28,14 @@ public class AnnotationRenameChunker extends Chunker {
 		HashMap<Sentence, AnnotationSet> chunkings = this.chunker.chunk(ps);
 
 		for (Sentence sentence : chunkings.keySet()){
-			
+			for (Annotation ann : chunkings.get(sentence).chunkSet()){
+				if ( this.rename.containsKey(ann.getType()) ){
+					ann.setType(this.rename.get(ann.getType()));
+				}
+			}
 		}
 		
 		return chunkings;
 	}
-	
+
 }
