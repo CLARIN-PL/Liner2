993882b9056b32467ecd682e94fad0a871d5341d
mihhauk <mihhauk@gmail.com>
2015-03-26 10:49:36 +0100

    Revert "dodanie sleepa przy zapytaniu o zadanie"
    This reverts commit 8df68d98758b249ab508f2f6d8d66aaa8f32fac7.
    
    revert - sleep nie pomaga

--- modules/g419-liner2-daemon/src/g419/liner2/daemon/FilebasedDaemonThread.java
+++ modules/g419-liner2-daemon/src/g419/liner2/daemon/FilebasedDaemonThread.java
@@ -75,11 +75,7 @@ public class FilebasedDaemonThread extends DaemonThread {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            try{
-                Thread.sleep(1000);
-            } catch (InterruptedException e){ e.printStackTrace();}
         }
-
     }
 
     private JSONObject checkForJob(){
