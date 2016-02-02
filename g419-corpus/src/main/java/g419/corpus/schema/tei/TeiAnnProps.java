5c327b102ec4f29586dd4f47075b620f2fffc92f
Michał Marcińczuk <marcinczuk@gmail.com>
2016-02-02 10:57:35 +0100

    Czytnik i piśnik dla props w formacie TEI.

---
+++ g419-corpus/src/main/java/g419/corpus/schema/tei/TeiAnnProps.java
@@ -0,0 +1,13 @@
+package g419.corpus.schema.tei;
+
+public class TeiAnnProps {
+
+	public static final String TAG_SEGMENT = "seg";
+	
+	public static final String TAG_FEATURE = "f";
+	
+	public static final String ATTR_CORESP = "coresp";
+	
+	public static final String ATTR_NAME = "name";
+	
+}
