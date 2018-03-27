a141e05759f9af024d6875c25fa0554927dcf1e2
Michał Marcińczuk <marcinczuk@gmail.com>
2018-03-27 21:46:09 +0200

    Change of libboost version to default

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo "Install dependencies"
-sudo apt-get install -y libboost1.58-all-dev libicu-dev libxml++2.6-dev bison flex libloki-dev cmake g++ swig python-dev
+sudo apt-get install -y libboost-all-dev libicu-dev libxml++2.6-dev bison flex libloki-dev cmake g++ swig python-dev
 
 mkdir install
 cd install
