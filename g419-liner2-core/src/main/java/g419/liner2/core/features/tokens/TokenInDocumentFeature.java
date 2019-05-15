b62f803774098141098ea7a1176d55f371f57841
gawor <micha.j.gawor@gmail.com>
2019-05-15 12:14:14 +0200

    TokenInDocumentFeature tracked

---
+++ g419-liner2-core/src/main/java/g419/liner2/core/features/tokens/TokenInDocumentFeature.java
@@ -0,0 +1,14 @@
+package g419.liner2.core.features.tokens;
+
+import g419.corpus.structure.Document;
+
+public abstract class TokenInDocumentFeature extends Feature {
+
+  public TokenInDocumentFeature(final String name) {
+    super(name);
+  }
+
+  public abstract void generate(Document document);
+
+}
+
