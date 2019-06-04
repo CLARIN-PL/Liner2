c9e21e3fa0535206af6c0a969d11f0ec136dae12
Michał Gawor <michal.gawor@pwr.edu.pl>
2019-06-04 15:40:59 +0200

    BaseCount added to tracked

---
+++ g419-liner2-core/src/main/java/g419/liner2/core/tools/BaseCount.java
@@ -0,0 +1,37 @@
+package g419.liner2.core.tools;
+
+import g419.corpus.structure.Document;
+import g419.corpus.structure.Sentence;
+import g419.corpus.structure.Token;
+import g419.corpus.structure.Paragraph;
+
+import java.util.HashMap;
+
+public class BaseCount {
+    private HashMap<String, Integer> baseCount = null;
+
+    public BaseCount(final Document document){
+        this.baseCount = new HashMap<>();
+
+        for (final Paragraph p : document.getParagraphs()) {
+            for (final Sentence s : p.getSentences()) {
+                for (final Token t : s.getTokens()) {
+                    final String lemma = t.getAttributeValue("base");
+                    if (this.baseCount.containsKey(lemma)) {
+                        this.baseCount.put(lemma, baseCount.get(lemma) + 1);
+                    } else {
+                        this.baseCount.put(lemma, 1);
+                    }
+                }
+            }
+        }
+    }
+
+    public int getBaseCount(final Token t){
+        return this.baseCount.get(t.getAttributeValue("base"));
+    }
+
+    public int getBaseCount(final String base){
+        return this.baseCount.get(base);
+    }
+}
