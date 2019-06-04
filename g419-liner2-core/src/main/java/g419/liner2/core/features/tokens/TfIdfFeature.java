742c099ae9b4370b0bfec7025e2f89f7d2e77fbe
Michał Gawor <michal.gawor@pwr.edu.pl>
2019-06-04 15:40:13 +0200

    BaseCount moved to tools

--- g419-liner2-core/src/main/java/g419/liner2/core/features/tokens/TfIdfFeature.java
+++ g419-liner2-core/src/main/java/g419/liner2/core/features/tokens/TfIdfFeature.java
@@ -1,5 +1,6 @@
 package g419.liner2.core.features.tokens;
 
+import g419.liner2.core.tools.BaseCount;
 import g419.corpus.structure.Document;
 import g419.corpus.structure.Paragraph;
 import g419.corpus.structure.Sentence;
@@ -11,14 +12,12 @@ import java.util.List;
 public class TfIdfFeature extends TokenInDocumentFeature {
 
   public TfIdfFeature(String name) {
-    super(name);
+      super(name);
   }
-
-  BaseNumberFeature baseNumberGenerator = new BaseNumberFeature("base_number");
 
   @Override
   public void generate(Document document) {
-    this.baseNumberGenerator.generate(document);
+    BaseCount baseCount = new BaseCount(document);
     int thisFeatureIdx = document.getAttributeIndex().getIndex(this.getName());
 
     float tokenNumber = (float) document.getTokenNumber();
@@ -26,15 +25,14 @@ public class TfIdfFeature extends TokenInDocumentFeature {
     for(Paragraph paragraph : document.getParagraphs()){
       for(Sentence sentence : paragraph.getSentences()){
         for(Token token : sentence.getTokens()){
-	  final String base = token.getAttributeValue("base");
-	  float idf = Float.parseFloat(token.getAttributeValue("key_value_idf_base"));
-	  float tf = (float) this.baseNumberGenerator.getBaseCount(base);
-	  float normTf = tf / tokenNumber;
-	  float tfIdf = normTf * idf;
-	  token.setAttributeValue(thisFeatureIdx, tfIdf > 0.05 ? "1" : "0");
-	}
+	      final String base = token.getAttributeValue("base");
+	      float idf = Float.parseFloat(token.getAttributeValue("key_value_idf_base"));
+	      float tf = baseCount.getBaseCount(base);
+	      float normTf = tf / tokenNumber;
+	      float tfIdf = normTf * idf;
+	      token.setAttributeValue(thisFeatureIdx, tfIdf > 0.05 ? "1" : "0");
+        }
       }
     }
   }
-
 }
