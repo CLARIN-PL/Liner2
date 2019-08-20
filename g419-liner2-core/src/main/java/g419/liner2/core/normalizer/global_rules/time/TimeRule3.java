96c57017a171e07d43e4b47487a28b9a12cecd73
gawor <micha.j.gawor@gmail.com>
2019-08-20 18:34:19 +0200

    AZON bugfix

--- g419-liner2-core/src/main/java/g419/liner2/core/normalizer/global_rules/time/TimeRule3.java
+++ g419-liner2-core/src/main/java/g419/liner2/core/normalizer/global_rules/time/TimeRule3.java
@@ -9,24 +9,30 @@ import java.util.List;
 public class TimeRule3 extends AbstractRule {
   @Override
   protected String doNormalize(String lval, String base, String previous, String first, String creationDate) {
-    String sign = "" + lval.charAt(0);
-    List<String> parts = new ArrayList<>(Arrays.asList(lval.substring(1, 11).split("-")));
-    if (parts.size() == 3) {
-      return toString(
-          addYears(
-              addMonths(
-                  addDays(
-                      fromString(creationDate),
-                      sign + parts.get(2)
-                  ),
-                  sign + parts.get(1)
-              ),
-              sign + parts.get(0)
-          )
-      ) + lval.substring(11);
+    if (lval.length() > 11) {
+      String sign = "" + lval.charAt(0);
+      List<String> parts = new ArrayList<>(Arrays.asList(lval.substring(1, 11).split("-")));
+      if (parts.size() == 3) {
+        return toString(
+                addYears(
+                        addMonths(
+                                addDays(
+                                        fromString(creationDate),
+                                        sign + parts.get(2)
+                                ),
+                                sign + parts.get(1)
+                        ),
+                        sign + parts.get(0)
+                )
+        ) + lval.substring(11);
+      }
     }
-    return null;
+    else {
+      System.out.println("Add support for lval [" + lval + "] of base [" + base + "]");
+    }
+    return null; 
   }
+
 
   @Override
   public boolean matches(String lval, String base) {
