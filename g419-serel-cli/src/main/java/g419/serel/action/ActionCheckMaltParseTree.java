bd0cca44a16ee891cba5279df13cb3f3c3b06aaa
Michał Olek <michal.olek@pwr.edu.pl>
2020-09-07 12:59:03 +0200

    writing results to a file when checking malt parser tree

--- g419-serel-cli/src/main/java/g419/serel/action/ActionCheckMaltParseTree.java
+++ g419-serel-cli/src/main/java/g419/serel/action/ActionCheckMaltParseTree.java
@@ -10,9 +10,14 @@ import g419.lib.cli.CommonOptions;
 import g419.liner2.core.tools.parser.MaltParser;
 import g419.liner2.core.tools.parser.MaltSentenceLink;
 import g419.serel.converter.DocumentToSerelExpressionConverter;
+import g419.serel.structure.ParseTreeMalfunction;
 import g419.serel.structure.SerelExpression;
 import org.apache.commons.cli.CommandLine;
+import javax.swing.text.html.parser.Parser;
+import java.io.FileWriter;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -56,68 +61,81 @@ public class ActionCheckMaltParseTree extends Action {
     final OutputStream os = WriterFactory.get().getOutputStreamFileOrOut(outputFilename);
     converter = new DocumentToSerelExpressionConverter(malt,null);
 
-    try (final AbstractDocumentReader reader = ReaderFactory.get().getStreamReader(inputFilename, inputFormat))
-         //final AbstractDocumentWriter writer = SerelWriterFactory.create(SerelOutputFormat.valueOf(outputFormat.toUpperCase()), os, malt,pw))
+    try (final AbstractDocumentReader reader = ReaderFactory.get().getStreamReader(inputFilename, inputFormat);
+         final PrintWriter writer = new PrintWriter( os) )
     {
-      reader.forEach(doc->checkMaltParseTree(doc));
+      List<ParseTreeMalfunction> result = new LinkedList<>();
+      reader.forEach(doc->result.addAll(checkMaltParseTree(doc)));
+      writer.println("Code\tdocument\tann_id\tsourceIndex\ttargetIndex\tstartAnnIndex\tendAnnIndex");
+      result.stream().forEach(line -> writer.println(line.getMalfunctionCode()+"\t"+
+                                                     line.getDocumentPath()+"\t"+
+                                                     line.getAnnotationId()+"\t"+
+                                                     line.getSourceIndex()+"\t"+
+                                                     line.getTargetIndex()+"\t"+
+                                                     line.getAnnStartRange()+"\t"+
+                                                     line.getAnnEndRange()
+      ));
     }
   }
 
-  private void checkMaltParseTree(Document document ) {
+  private List<ParseTreeMalfunction> checkMaltParseTree(Document document ) {
+    List<ParseTreeMalfunction> result = new LinkedList<>();
 
     final List<SerelExpression> serelExpressions = converter.convert(document);
     for(SerelExpression se : serelExpressions ) {
 
       Annotation aFrom = se.getRelation().getAnnotationFrom();
-      isAnnotationHeadPointingOut(se, aFrom);
-      if (isHavingMoreElementsPointingOut(se,aFrom)) {
-        System.out.println(" DOC: "+ se.getSentence().getDocument().getName()+   " ann = "+aFrom.getId());
-      }
+      result.addAll(isAnnotationHeadPointingOut(se, aFrom));
+      result.addAll(isHavingMoreElementsPointingOut(se, aFrom));
 
       Annotation aTo = se.getRelation().getAnnotationTo();
-      isAnnotationHeadPointingOut(se, aTo);
-      if (isHavingMoreElementsPointingOut(se,aFrom)) {
-        System.out.println(" DOC: "+ se.getSentence().getDocument().getName()+   " ann = "+aTo.getId());
-      }
-
-
-
+      result.addAll(isAnnotationHeadPointingOut(se, aTo));
+      result.addAll(isHavingMoreElementsPointingOut(se, aFrom));
     }
-
-
-    //Widać, że nie zawsze głowa jest elementem "wychodzącym" z nazwy, np.
-    //"location: nam_org_organization -> wielki -> finał -> z -> problem <- w <- Wrocław <- nam_loc_gpe_city"
-    //    |
-    //    |
-    //    V
-    // bierzemy node i sprawdzamy czy link z "głowy" wychodzi ma zewnątrz
-
-    // 2. sprawdzenie czy są przypadki, gdzie w relacji była nazwa wielowyrazowa
-    // i oba elementy nazwy wskazywały na element poza nazwą
-    //    |
-    //    |
-    //    V
-    // bierzemy node, jeśli nazwa jest wielowyrazowa to sprawdzamy czy nie jest czasem
-    // tak, że więcej niż jeden wyraz wskazuje na zewnątrz "poza" nazwą
-
+    return result;
   }
 
+  /*
+  Widać, że nie zawsze głowa jest elementem "wychodzącym" z nazwy, np.
+  "location: nam_org_organization -> wielki -> finał -> z -> problem <- w <- Wrocław <- nam_loc_gpe_city" :
+  bierzemy node i sprawdzamy czy link z "głowy" wychodzi ma zewnątrz
+  */
+  private List<ParseTreeMalfunction> isAnnotationHeadPointingOut(SerelExpression se, Annotation a) {
 
-  private boolean isAnnotationHeadPointingOut(SerelExpression se, Annotation a) {
+    List<ParseTreeMalfunction> result = new LinkedList<>();
+
     int headForOut = a.getHead();
     Optional<MaltSentenceLink> linkForOut = se.getMaltSentence().getLinksBySourceIndex(headForOut);
     if(linkForOut.isPresent()) {
       int targetOutIndex = linkForOut.get().getTargetIndex();
       if (a.isTokenIndexWithin(targetOutIndex)) {
-        System.out.println(" DOC: "+ se.getSentence().getDocument().getName()+   " headIndex = "+headForOut+ " headToken=" +a.getHeadToken().getOrth()   + " targetOutIndex =" + targetOutIndex+ " ["+a.getBegin()+":"+a.getEnd()+"] "+a.getText());
-        return false;
+
+        ParseTreeMalfunction ptm = ParseTreeMalfunction.builder()
+            .malfunctionCode(ParseTreeMalfunction.MalfunctionCode.AHPI)
+            .documentPath(se.getSentence().getDocument().getName())
+            .annotationId(a.getId())
+            .sourceIndex(headForOut)
+            .targetIndex(targetOutIndex)
+            .annStartRange(a.getBegin())
+            .annEndRange((a.getEnd()))
+            .build();
+        result.add(ptm);
       }
     }
-    return true;
+    return result;
   }
 
-  private boolean isHavingMoreElementsPointingOut(SerelExpression se,Annotation ann) {
-    List<MaltSentenceLink> result = ann.getTokens()
+
+  /*
+   2. sprawdzenie czy są przypadki, gdzie w relacji była nazwa wielowyrazowa
+   i oba elementy nazwy wskazywały na element poza nazwą :
+   bierzemy node, jeśli nazwa jest wielowyrazowa to sprawdzamy czy nie jest czasem
+   tak, że więcej niż jeden wyraz wskazuje na zewnątrz "poza" nazwą
+  */
+  private List<ParseTreeMalfunction> isHavingMoreElementsPointingOut(SerelExpression se, Annotation ann) {
+    List<ParseTreeMalfunction> result = new LinkedList<>();
+
+    List<MaltSentenceLink> links = ann.getTokens()
         .stream()
         .map(index -> se.getMaltSentence().getLinksBySourceIndex(index))
         .filter(optLink -> optLink.isPresent())
@@ -125,14 +143,22 @@ public class ActionCheckMaltParseTree extends Action {
         .map(optLink -> optLink.get())
         .collect(Collectors.toList());
 
-    if (result.size() > 1) {
-      System.out.println("Ann =" + ann);
-      for (MaltSentenceLink maltSentenceLink : result) {
-        System.out.println("\tlink = " + maltSentenceLink);
+    if (links.size() > 1) {
+      for (MaltSentenceLink maltSentenceLink : links) {
+        ParseTreeMalfunction ptm = ParseTreeMalfunction.builder()
+            .malfunctionCode(ParseTreeMalfunction.MalfunctionCode.MEPO)
+            .documentPath(se.getSentence().getDocument().getName())
+            .annotationId(ann.getId())
+            .sourceIndex(maltSentenceLink.getSourceIndex())
+            .targetIndex(maltSentenceLink.getTargetIndex())
+            .annStartRange(ann.getBegin())
+            .annEndRange((ann.getEnd()))
+            .build();
+
+        result.add(ptm);
       }
-      return true;
     }
-    return false;
+    return result;
   }
 
 
